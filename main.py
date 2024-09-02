205c8080b8bb3ea5f8344862c9b95d75664a72a2
mstanl3y <12664948+mstanl3y@users.noreply.github.com>
2024-09-02 14:05:50 -0400

    implement report function

--- main.py
+++ main.py
@@ -21,22 +21,31 @@ def get_char_stats(text):
     lower_text = text.lower().strip()
     words = lower_text.split()
     full_text = "".join(words)
-    print(full_text)
+    #print(full_text)
 
     for char in full_text:
         if char in string.ascii_lowercase:
             char_count[char] += 1
 
-    return char_count    
+    return char_count
+
+def print_report(text):
+    char_dict = get_char_stats(text)
+
+    print(f"--- Begin report for {book_path}")
+
+    wc = count_words(text)
+    print(f"{wc} words found in the document.\n")
+
+    sorted_chars_dict = sorted(char_dict.items(), key=lambda x:x[1], reverse=True)
+    
+    for char in sorted_chars_dict:
+        print(f"The '{char[0]}' character was found {char[1]} times")
+
+    print("--- End report ---")
 
 def main():
-    text = get_book_text(book_path)
-    wc = count_words(text)
-    print(f"{wc} words found in {book_path}")
-
-    char_count = get_char_stats(text)
-    print(f"{char_count}")
-
+    print_report(get_book_text(book_path))
 
 if __name__ == '__main__': 
         main()

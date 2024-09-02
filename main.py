674bded3802ed550900baef3829ec4e52d96e414
mstanl3y <12664948+mstanl3y@users.noreply.github.com>
2024-09-02 13:35:06 -0400

    add get_char_stats function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import string
 
 book_path = "books/frankenstein.txt"
 
@@ -10,11 +11,31 @@ def count_words(text):
 def get_book_text(path):
      with open(book_path) as f:
         return f.read()
+     
+def get_char_stats(text):
+    char_count = {}
+
+    for char in string.ascii_lowercase:
+        char_count[char] = 0
+    
+    lower_text = text.lower().strip()
+    words = lower_text.split()
+    full_text = "".join(words)
+    print(full_text)
+
+    for char in full_text:
+        if char in string.ascii_lowercase:
+            char_count[char] += 1
+
+    return char_count    
 
 def main():
     text = get_book_text(book_path)
     wc = count_words(text)
     print(f"{wc} words found in {book_path}")
+
+    char_count = get_char_stats(text)
+    print(f"{char_count}")
 
 
 if __name__ == '__main__': 
